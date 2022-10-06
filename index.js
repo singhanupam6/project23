0430b3fc757a66d2ffbb24e2906fc9021d862ca9
singhanupam6 <anupamrajawat4@gmail.com>
2022-10-06 12:52:20 +0530

    loop

---
+++ index.js
@@ -0,0 +1,17 @@
+for(var count=0; count<5; count++)
+{
+    let count=prompt("enter your name", "");
+    document.write(count)
+}
+
+
+
+var total=0;
+for(var count=0; count<5; count++)
+{
+    var marks=prompt("enter marks");
+    total=total+parseInt(marks);
+    
+}
+console.log(total)
+var text=""
