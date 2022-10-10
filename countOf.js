da0057b98f38d1039506975bc8af27d4f8dcff18
singhanupam6 <anupamrajawat4@gmail.com>
2022-10-10 13:39:11 +0530

    project23

---
+++ countOf.js
@@ -0,0 +1,5 @@
+for(var count=0; count<5; count++)
+{
+    let count=prompt("enter your name", "");
+    document.write(count)
+}
--- marksOfStudents.js
+++ marksOfStudents.js
@@ -1,11 +1,3 @@
-for(var count=0; count<5; count++)
-{
-    let count=prompt("enter your name", "");
-    document.write(count)
-}
-
-
-
 var total=0;
 for(var count=0; count<5; count++)
 {
